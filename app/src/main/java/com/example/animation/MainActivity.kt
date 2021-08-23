f7eb9dc1313b5a708d4970e6366fc1a64e6ad4b5
AbdAlrahmanElsayed77 <abdoe3303@gmail.com>
2021-08-23 21:34:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.animation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mickey_image_1.animate()
+                .rotation(360f)
+                .translationXBy(-1000f)
+                .translationYBy(-1000f)
+                .duration = 3000
+
+        poke_image_2.animate()
+                .rotation(360f)
+                .translationXBy(1000f)
+                .translationYBy(1000f)
+                .duration = 3000
+
+        // Clicked
+        btn.setOnClickListener {
+            mickey_image_1.animate()
+                    .rotation(360f)
+                    .translationXBy(1000f)
+                    .translationYBy(1000f)
+                    .scaleX(0.5f)
+                    .scaleY(0.5f)
+                    .duration = 3000
+
+            poke_image_2.animate()
+                    .rotation(360f)
+                    .translationXBy(-1000f)
+                    .translationYBy(-1000f)
+                    .scaleX(0.5f)
+                    .scaleY(0.5f)
+                    .duration = 3000
+        }
+    }
+}
